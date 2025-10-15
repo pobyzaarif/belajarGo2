ab45e43ffd72d454c1aa5fb41de3fe4149ab8d80
Poby Zaarifwandono <pobyzarif34@gmail.com>
2025-10-16 00:08:09 +0700

    add db con n env example

--- app/http-server/main.go
+++ app/http-server/main.go
@@ -23,12 +23,21 @@ type Config struct {
 	AppHost string `env:"APP_HOST"`
 	AppPort string `env:"APP_PORT"`
 
-	DBDriver        string `env:"DB_DRIVER"`
+	DBDriver string `env:"DB_DRIVER"`
+
 	DBMySQLHost     string `env:"DB_MYSQL_HOST"`
 	DBMySQLPort     string `env:"DB_MYSQL_PORT"`
 	DBMySQLUser     string `env:"DB_MYSQL_USER"`
 	DBMySQLPassword string `env:"DB_MYSQL_PASSWORD"`
 	DBMySQLName     string `env:"DB_MYSQL_NAME"`
+
+	DBSQLiteName string `env:"DB_SQLITE_NAME"`
+
+	DBPostgreSQLHost     string `env:"DB_POSTGRESQL_HOST"`
+	DBPostgreSQLPort     string `env:"DB_POSTGRESQL_PORT"`
+	DBPostgreSQLUser     string `env:"DB_POSTGRESQL_USER"`
+	DBPostgreSQLPassword string `env:"DB_POSTGRESQL_PASSWORD"`
+	DBPostgreSQLName     string `env:"DB_POSTGRESQL_NAME"`
 }
 
 func main() {
@@ -44,12 +53,18 @@ func main() {
 
 	// Init db connection
 	databaseConfig := database.Config{
-		DBDriver:        config.DBDriver,
-		DBMySQLHost:     config.DBMySQLHost,
-		DBMySQLPort:     config.DBMySQLPort,
-		DBMySQLUser:     config.DBMySQLUser,
-		DBMySQLPassword: config.DBMySQLPassword,
-		DBMySQLName:     config.DBMySQLName,
+		DBDriver:             config.DBDriver,
+		DBMySQLHost:          config.DBMySQLHost,
+		DBMySQLPort:          config.DBMySQLPort,
+		DBMySQLUser:          config.DBMySQLUser,
+		DBMySQLPassword:      config.DBMySQLPassword,
+		DBMySQLName:          config.DBMySQLName,
+		DBSQLiteName:         config.DBSQLiteName,
+		DBPostgreSQLHost:     config.DBPostgreSQLHost,
+		DBPostgreSQLPort:     config.DBPostgreSQLPort,
+		DBPostgreSQLUser:     config.DBPostgreSQLUser,
+		DBPostgreSQLPassword: config.DBPostgreSQLPassword,
+		DBPostgreSQLName:     config.DBPostgreSQLName,
 	}
 	db := databaseConfig.GetDatabaseConnection()
 	logger.Info("Database client connected!")
