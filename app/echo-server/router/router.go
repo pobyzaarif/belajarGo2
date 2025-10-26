487a4607afd367b3c6ecd657a69f39f0de42ed1f
Poby Zaarifwandono <pobyzarif34@gmail.com>
2025-10-26 23:51:54 +0700

    rename rbac to acl n simplify role matcher

--- app/echo-server/router/router.go
+++ app/echo-server/router/router.go
@@ -22,21 +22,31 @@ func RegisterPath(
 		})
 	})
 
+	// Init JWT
 	jwtMiddleware := middleware.JWTMiddleware(jwtSecret)
-	userNAdmin := middleware.RBACMiddleware([]string{"user", "admin"})
-	adminOnly := middleware.RBACMiddleware([]string{"admin"})
-	superadminOnly := middleware.RBACMiddleware([]string{"superadmin"})
 
-	// user endpoint
+	// Init ACL
+	userNAdminAccess := middleware.ACLMiddleware(map[string]bool{
+		"admin": true,
+		"user":  true,
+	})
+	adminAccess := middleware.ACLMiddleware(map[string]bool{
+		"admin": true,
+	})
+	superadminAccess := middleware.ACLMiddleware(map[string]bool{
+		"superadmin": true,
+	})
+
+	// User endpoint
 	userEndpoint := e.Group("/users")
 	userEndpoint.POST("/register", ctrlUser.Register)
 	userEndpoint.POST("/login", ctrlUser.Login)
 
-	// inventory endpoint
+	// Inventory endpoint
 	inventoryEndpoint := e.Group("/inventories", jwtMiddleware)
-	inventoryEndpoint.GET("", ctrlInv.GetAll, userNAdmin)
-	inventoryEndpoint.GET("/:code", ctrlInv.GetByCode, userNAdmin)
-	inventoryEndpoint.POST("", ctrlInv.Create, adminOnly)
-	inventoryEndpoint.PUT("/:code", ctrlInv.Update, adminOnly)
-	inventoryEndpoint.DELETE("/:code", ctrlInv.Delete, superadminOnly)
+	inventoryEndpoint.GET("", ctrlInv.GetAll, userNAdminAccess)
+	inventoryEndpoint.GET("/:code", ctrlInv.GetByCode, userNAdminAccess)
+	inventoryEndpoint.POST("", ctrlInv.Create, adminAccess)
+	inventoryEndpoint.PUT("/:code", ctrlInv.Update, adminAccess)
+	inventoryEndpoint.DELETE("/:code", ctrlInv.Delete, superadminAccess)
 }
