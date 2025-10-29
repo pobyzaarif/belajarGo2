a4a7470dab14efed4c0314b19960300222013b3d
Poby Zaarifwandono <pobyzarif34@gmail.com>
2025-10-29 15:10:11 +0700

    explore rafly n halim

--- app/echo-server/middleware/middleware.go
+++ app/echo-server/middleware/middleware.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -82,4 +83,10 @@ func ACLMiddleware(rolesMap map[string]bool) echo.MiddlewareFunc {
 			return forbiddenResponse(c)
 		}
 	}
+}
+
+func JwtEchoMiddleware(jwtSign string) echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(jwtSign),
+	})
 }
